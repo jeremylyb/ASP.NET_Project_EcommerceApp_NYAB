b69d43335bfd48950fa969b96b99d0a8cc1b78ae
jeremylyb <96461868+jeremylyb@users.noreply.github.com>
2024-03-26 13:36:06 +0800

    26 March 2024 - ProductsRestApi
    - Set up ProductsRestApi
    - Provide set up notes for RestAPI setup and controller setup

---
+++ NotYourAverageBicepShoppingApp/Controllers/ProductsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NotYourAverageBicepShoppingApp.ProductRestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
